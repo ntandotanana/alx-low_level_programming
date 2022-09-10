0df9c0b27f3b9ca7f61805de6e48530e75d6edf8
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-10 05:23:06 -0700

    low level programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
 /**
- * main - print if the number is postive, zero, or negative
+ * main - print if the number is positive, zero, or negative
  * Description: program prints "Programming is positive, zero, or negative"
  * Return: 0
  */
 int main(void)
 {
 	char ch;
-	
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	for(ch +'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
-	return (0);
+	return(0)
 }
