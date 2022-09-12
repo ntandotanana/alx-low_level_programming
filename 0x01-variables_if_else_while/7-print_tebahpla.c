33bed2d483af3f9452fc220a0134fdbea9d5bd2c
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-12 01:18:31 -0700

    low level programming

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number positive, zero, or negative
+ * Description: program prints "Programming is positive, zero, or negative"
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
