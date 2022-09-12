2db58a00d104f1ab2f1bbbd6556e3225a0c2cdf4
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-12 01:11:31 -0700

    low level programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ * Description: program prints "Programming is positive, zero, or negative"
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+	putchar(n);
+}
+putchar('\n');
+return (0);
+}
