c9c99a39c794d1ae26432bc410022defca22fbc4
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-13 02:32:23 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+	putchar(n);
+}
+putchar('\n');
+}
