77de739bd0481c59e293e80cf24363dfbe972904
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-14 03:05:37 -0700

    nested llops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * print_alphabet - should check the code
+ * print_alphabet - function to print abc
  *
- * Return: void
+ * Return: 0
  */
 void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-{
-	_putchar(c);
+	char c;
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
-_putchar('\n');
-}
+
