05c46cd325328cb89e030c0ad6b3baad2c94c631
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-22 14:29:40 +0200

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,11 +3,8 @@
 /**
 * _strcmp - Function which compare two strings
 * @s1: first string
-* @s2:second string
-* Return:
-* returns zero if s1 == s2
-* returns negative number if s1 < s2
-* returns positive number if s1 > s2
+* @s2: second string
+* Return: returns zero if s1 == s2
 */
 
 int _strcmp(char *s1, char *s2)
