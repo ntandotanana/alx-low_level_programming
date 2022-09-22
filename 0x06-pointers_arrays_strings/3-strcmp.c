d6fe230e5555e9068ab4997bf57af91f94d9ad75
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-22 14:32:16 +0200

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,20 +16,24 @@ while (1)
 {
 if (s1[i] == '\0' && s2[i] == '\0')
 break;
+
 else if (s1[i] == '\0')
 {
 diff = s2[i];
 break;
 }
+
 else if (s2[i] == '\0')
 {
 diff = s1[i];
 break;
 }
+
 else if (s1[i] != s2[i])
 {
 diff = s1[i] - s2[i];
 break;
+
 }
 else
 i++;
