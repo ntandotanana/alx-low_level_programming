ffc0d71480a06331c8c693788733989d29757989
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-22 14:19:48 +0200

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *_strcat - concatenates  the string pointed to @src 
@@ -11,17 +12,14 @@
 
 char *_strcat(char *dest, char *src)
 {
+
 int index = 0, dest_len = 0;
 
 while (dest[index++])
-{
 dest_len++;
-}
 
 for (index = 0; src[index]; index++)
-{
 dest[dest_len++] = src[index];
-}
 
 return (dest);
 }
