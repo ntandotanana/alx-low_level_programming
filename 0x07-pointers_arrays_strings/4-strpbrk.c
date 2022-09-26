cce43c0588fafe757e78caef7691d38c2ad7fccb
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-26 15:11:38 +0200

    Update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,18 +7,18 @@
  * @accept: input
  * Return: Always 0 (success)
  */
-char *strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-int i, n;
+    int i, n;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-    for (n = 0; accept[n] != '\0'; n++)
+    for (i = 0; s[i] != '\0'; i++)
     {
-        if (s[i] == accept[n])
-        return (s + i);
+        for (n = 0; accept[n] != '\0'; n++)
+        {
+            if (s[i] == accept[n])
+            return (s + i);
+        }
     }
-}
 
-return (NULL);
+    return (NULL);
 }
