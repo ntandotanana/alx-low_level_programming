6863289dd35fcb66e6c356d5e76e88c72c5e5fa3
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-26 13:37:51 +0200

    pointers and arrays

--- 0X07-pointers_arrays_strings/main.h
+++ 0X07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char );
-char *_memset(char *s, char , unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char );
 unsigned int _strspn(char *s, char *accept);
