dc6760e8ad086122c103ed551e7439cb9e2f81a0
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-26 15:03:07 +0200

    updaet betty coding style

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, n;
+int i, n;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (n = 0; n < 8; n++)
-        _putchar(a[i][n]);
-        _putchar('\n');
-    }
+for (i = 0; i < 8; i++)
+{
+for (n = 0; n < 8; n++)
+_putchar(a[i][n]);
+_putchar('\n');
+}
 }
