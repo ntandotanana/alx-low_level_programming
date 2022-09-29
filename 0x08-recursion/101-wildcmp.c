1045abadb935521a2106b8b70866bf616a9188ef
ntandotanana <ntando.tanana1996@gmail.com>
2022-09-29 15:54:22 +0200

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,13 +13,13 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (!*s1 && !*s2)
 		return (1);
-	if (s2 == '')
+	if (s2 == ' ')
 	{
-		if ((s2 + 1) != *s1 && *(s2 + 1) != '')
+		if ((s2 + 1) != *s1 && *(s2 + 1) != ' ')
 		{
 			return (wildcmp(s1 + 1, s2));
 		}
-		else if ((s2 + 1) != *s1 && *(s2 + 1) == '')
+		else if ((s2 + 1) != *s1 && *(s2 + 1) == ' ')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
