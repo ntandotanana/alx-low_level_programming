4e18258136186baf0cb0e56934e9f2dd90485e2d
ntandotanana <ntando.tanana1996@gmail.com>
2022-10-10 17:28:17 +0200

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: prints the name of the file it was compiled from
- * Return: Always 0 (Succes)
+ * main - prints the name of the file
+ * Return: Always 0
  */
 int main(void)
 {
-    printf("%s/n", __FILE__);
+    _putchar("%s\n", __FILE__);
     return (0);
 }
